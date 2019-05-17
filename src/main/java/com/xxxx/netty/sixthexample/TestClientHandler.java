8235b437bf537037e17d37569b2b97ae49293eb7
xieyaqi <987159036@qq.com>
2019-05-17 17:58:24 +0800

    add enum

--- src/main/java/com/xxxx/netty/sixthexample/TestClientHandler.java
+++ src/main/java/com/xxxx/netty/sixthexample/TestClientHandler.java
@@ -3,26 +3,54 @@ package com.xxxx.netty.sixthexample;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.util.Random;
+
 /**
  * @author xieyaqi
  * @mail 987159036@qq.com
  * @date 2019-05-17 16:11
  */
-public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        MyDataInfo.Person person = MyDataInfo.Person.newBuilder()
-                .setName("张三")
-                .setAge(20)
-                .setAddress("北京")
-                .build();
+        int randomInt = new Random(System.currentTimeMillis()).nextInt(3);
 
-        ctx.channel().writeAndFlush(person);
+        MyDataInfo.MyMessage message = null;
+
+        if (0 == randomInt) {
+            message = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.PersonType)
+                    .setPerson(MyDataInfo.Person.newBuilder()
+                            .setName("张三")
+                            .setAge(20)
+                            .setAddress("北京")
+                            .build())
+                    .build();
+        } else if (1 == randomInt) {
+            message = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.DogType)
+                    .setDog(MyDataInfo.Dog.newBuilder()
+                            .setName("Dog")
+                            .setAge(20)
+                            .build())
+                    .build();
+        } else {
+            message = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.CatType)
+                    .setCat(MyDataInfo.Cat.newBuilder()
+                            .setName("Dog")
+                            .setCity("北京")
+                            .build())
+                    .build();
+        }
+
+
+        ctx.channel().writeAndFlush(message);
     }
 }
