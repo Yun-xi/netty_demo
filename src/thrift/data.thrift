42574d080e67276c44a24ed8caa9f897a67bf132
XieYaqi <987159036@qq.com>
2019-05-19 23:28:51 +0800

    add thrift dependcy

---
+++ src/thrift/data.thrift
@@ -0,0 +1,29 @@
+namespace java thrift.generated
+
+#别名
+typedef i16 short
+typedef i32 int
+typedef i64 long
+typedef bool boolean
+typedef string String
+
+#domain
+struct Person {
+    1: optional String username,
+    2: optional int age,
+    3: optional boolean married
+}
+
+#异常
+exception DataException {
+    1: optional String message,
+    2: optional String callStack,
+    3: optional String data
+}
+
+#接口
+service PersonService {
+    Person getPersonByUsername(1: required String username) throws (1: DataException dataException),
+
+    void savePerson(1: required Person person) throws (1: DataException dataException)
+}
