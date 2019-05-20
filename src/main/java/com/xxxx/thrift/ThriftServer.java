2886921f271c128724dfd8c766809bc188d00897
xieyaqi <987159036@qq.com>
2019-05-20 17:38:41 +0800

    加注释

--- src/main/java/com/xxxx/thrift/ThriftServer.java
+++ src/main/java/com/xxxx/thrift/ThriftServer.java
@@ -12,13 +12,13 @@ import thrift.generated.PersonService;
 public class ThriftServer {
 
     public static void main(String[] args) throws TTransportException {
-        TNonblockingServerSocket socket = new TNonblockingServerSocket(8899);
+        TNonblockingServerSocket socket = new TNonblockingServerSocket(8899);   // socket
         THsHaServer.Args arg = new THsHaServer.Args(socket).minWorkerThreads(2).maxWorkerThreads(4);
         PersonService.Processor<PersonServiceImpl> processor = new PersonService.Processor<>(new PersonServiceImpl());
 
-        arg.protocolFactory(new TCompactProtocol.Factory());
-        arg.transportFactory(new TFramedTransport.Factory());
-        arg.processorFactory(new TProcessorFactory(processor));
+        arg.protocolFactory(new TCompactProtocol.Factory());    // 协议
+        arg.transportFactory(new TFramedTransport.Factory());   // 传输
+        arg.processorFactory(new TProcessorFactory(processor)); // 处理器
 
         TServer server = new THsHaServer(arg);
 
