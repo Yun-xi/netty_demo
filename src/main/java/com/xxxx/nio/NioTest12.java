2492450319e594e7d7373cc3576bc0f44c8742d1
xieyaqi <987159036@qq.com>
2019-06-04 17:39:45 +0800

    add selector

--- src/main/java/com/xxxx/nio/NioTest12.java
+++ src/main/java/com/xxxx/nio/NioTest12.java
@@ -3,8 +3,10 @@ package com.xxxx.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NioTest12 {
 
@@ -25,6 +27,63 @@ public class NioTest12 {
             ServerSocket serverSocket = serverSocketChannel.socket();
             InetSocketAddress address = new InetSocketAddress(ports[i]);
             serverSocket.bind(address);
+
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("监听端口: "+ ports[i]);
+        }
+
+        while (true) {
+            int numbers = selector.select();
+            System.out.println("numbers: " + numbers);
+
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+            System.out.println("selectionKeys: " + selectionKeys);
+
+            Iterator<SelectionKey> iter = selectionKeys.iterator();
+
+            while (iter.hasNext()) {
+                SelectionKey selectionKey = iter.next();
+
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel)selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+
+                    iter.remove();
+
+                    System.out.println("获得客户端连接: " + socketChannel);
+                }  else if (selectionKey.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel)selectionKey.channel();
+
+                    int byteRead = 0;
+
+                    while (true) {
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+                        byteBuffer.clear();
+
+                        int read = socketChannel.read(byteBuffer);
+
+                        if (read <= 0) {
+                            break;
+                        }
+
+                        byteBuffer.flip();
+
+                        socketChannel.write(byteBuffer);
+
+                        byteRead += read;
+                    }
+
+                    System.out.println("读取: " + byteRead + ", 来自于: " + socketChannel);
+
+                    iter.remove();
+                }
+            }
         }
 
     }
