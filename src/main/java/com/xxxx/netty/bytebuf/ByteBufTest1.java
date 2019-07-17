b3a3c363cf12a10867da4ae150329d78df9ad76f
XieYaqi <987159036@qq.com>
2019-07-17 18:50:36 +0800

    Netty ByteBufTest

---
+++ src/main/java/com/xxxx/netty/bytebuf/ByteBufTest1.java
@@ -0,0 +1,39 @@
+package com.xxxx.netty.bytebuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.Charset;
+
+public class ByteBufTest1 {
+
+    public static void main(String[] args) {
+        ByteBuf byteBuf = Unpooled.copiedBuffer("你hello world", Charset.forName("utf-8"));
+
+        // 如果是堆上的缓冲返回true
+        if (byteBuf.hasArray()) {
+            byte[] content = byteBuf.array();
+            System.out.println(new String(content, Charset.forName("utf-8")));
+
+            System.out.println(byteBuf);
+
+            // 第一个字节的偏移量
+            System.out.println(byteBuf.arrayOffset());
+
+            System.out.println(byteBuf.readerIndex());
+            System.out.println(byteBuf.writerIndex());
+            System.out.println(byteBuf.capacity());
+
+            // 可读的字节数（writerIndex - readerIndex）
+            int length = byteBuf.readableBytes();
+            System.out.println(length);
+
+            for (int i = 0; i < byteBuf.readableBytes(); ++i) {
+                System.out.println((char)byteBuf.getByte(i));
+            }
+
+            System.out.println(byteBuf.getCharSequence(0, 4, Charset.forName("utf-8")));
+            System.out.println(byteBuf.getCharSequence(4, 6, Charset.forName("utf-8")));
+        }
+    }
+}
